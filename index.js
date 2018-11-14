f767166a3cad9744e7af6203015d4dc13aa23454
Matt Eubanks <spc.eubanks.matthew@gmail.com>
2018-11-14 13:15:54 -0700

    change font size in about

--- index.js
+++ index.js
@@ -17,30 +17,3 @@ function myFunction() {
     header.classList.remove('sticky');
   }
 }
-
-// function smoothScroll() {
-//   $('a').on('click', function(event) {
-
-//     if (this.hash !== '') {
-
-//       event.preventDefault();
-
-//       var hash = this.hash;
-
-//       $('html, body').animate(
-//         {
-//           scrollTop: $(hash).offset().top,
-//         },
-//         800,
-//         function() {
-
-//           window.location.hash = hash;
-//         }
-//       );
-//     }
-//   });
-// }
-
-// function eventHandlers() {
-//   smoothScroll();
-// }
